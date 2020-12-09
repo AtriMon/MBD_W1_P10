efe0746fdd710458e801c8e99fdb19da957884c2
AtriMon <37790153+AtriMon@users.noreply.github.com>
2020-12-09 16:51:01 +0530

    Optimized Initialization

--- MBD_W1_P10_init.m
+++ MBD_W1_P10_init.m
@@ -1,9 +1,10 @@
 %Initialization
+%Input
 V1=-5:0.1:0;
 V2=0.1:0.1:5;
 V = [V1 V2]
 V = V';
-
+%Output for Si PN Junction Diode
 I1 = 0.0075*(exp(V1/0.0259)-1);
 I2 = 0.0075*(exp(V2/0.0259)-1);
 I = [I1 I2];
